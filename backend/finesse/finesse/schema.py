e4ea80e3c6e8c7168721a5a66ab177178d622a6d
Sam Masto <56971406+smashed-toes@users.noreply.github.com>
2023-10-18 18:36:31 -0700

    Backend setup with product model in Django and cra with typescript

---
+++ backend/finesse/finesse/schema.py
@@ -0,0 +1,30 @@
+from graphene import Argument, Field, ID, ObjectType, Schema, List
+from graphene_django import DjangoConnectionField
+from product.models import Product, Image
+from graphene_django import DjangoObjectType, DjangoListField
+
+class ImageType(DjangoObjectType):
+    class Meta:
+        model = Image
+        field = ('id', 'image')
+        use_connection = True
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+        field = ('id', 'title', 'price', 'model_info', 'material_info')
+        use_connection = True
+    images = DjangoListField(ImageType)
+
+class Query(ObjectType):
+    products = DjangoConnectionField(ProductType)
+    product = Field(ProductType, id=Argument(ID, required=True))
+
+    def resolve_products(root, info, **kwargs):
+        return Product.objects.all()
+    
+    def resolve_product(root, info, **kwargs):
+        return Product.objects.get(id=kwargs.get('id'))
+    
+
+schema = Schema(query=Query)
---
+++ backend/finesse/product/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    # add uuid?
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    model_info = models.TextField(null=True, default=None)
+    material_info = models.TextField(default='There is no info on the materials')
+
+
+    def __str__(self):
+        return self.title
+    
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+
+    def __str__(self):
+        return f"Image of {self.product.title}"
